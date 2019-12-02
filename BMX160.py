dfb5d1ece0e115376677301c35b53e32b5a7fa91
tomer arnon <tomerarnon@gmail.com>
2019-12-02 15:45:41 -0800

    add some convenient returns. More named registers, both I2C addresses

--- BMX160.py
+++ BMX160.py
@@ -32,15 +32,15 @@ BMX160_ACCEL_CONFIG_ADDR   = const(0x40)
 BMX160_ACCEL_RANGE_ADDR    = const(0x41)
 BMX160_GYRO_CONFIG_ADDR    = const(0x42)
 BMX160_GYRO_RANGE_ADDR     = const(0x43)
-BMX160_AUX_ODR_ADDR        = const(0x44)
+BMX160_MAG_ODR_ADDR        = const(0x44)
 BMX160_FIFO_DOWN_ADDR      = const(0x45)
 BMX160_FIFO_CONFIG_0_ADDR  = const(0x46)
 BMX160_FIFO_CONFIG_1_ADDR  = const(0x47)
-BMX160_AUX_IF_0_ADDR       = const(0x4B)
-BMX160_AUX_IF_1_ADDR       = const(0x4C)
-BMX160_AUX_IF_2_ADDR       = const(0x4D)
-BMX160_AUX_IF_3_ADDR       = const(0x4E)
-BMX160_AUX_IF_4_ADDR       = const(0x4F)
+BMX160_MAG_IF_0_ADDR       = const(0x4B)
+BMX160_MAG_IF_1_ADDR       = const(0x4C)
+BMX160_MAG_IF_2_ADDR       = const(0x4D)
+BMX160_MAG_IF_3_ADDR       = const(0x4E)
+BMX160_MAG_IF_4_ADDR       = const(0x4F)
 BMX160_INT_ENABLE_0_ADDR   = const(0x50)
 BMX160_INT_ENABLE_1_ADDR   = const(0x51)
 BMX160_INT_ENABLE_2_ADDR   = const(0x52)
@@ -94,15 +94,75 @@ BMX160_GYRO_SUSPEND_MODE             = const(0x14)
 BMX160_GYRO_NORMAL_MODE              = const(0x15)
 BMX160_GYRO_FASTSTARTUP_MODE         = const(0x17)
 
-# Aux power mode
-BMX160_AUX_SUSPEND_MODE              = const(0x18)
-BMX160_AUX_NORMAL_MODE               = const(0x19)
-BMX160_AUX_LOWPOWER_MODE             = const(0x1A)
+# Mag power mode
+BMX160_MAG_SUSPEND_MODE              = const(0x18)
+BMX160_MAG_NORMAL_MODE               = const(0x19)
+BMX160_MAG_LOWPOWER_MODE             = const(0x1A)
 
+# Delay in ms settings
+BMX160_ACCEL_DELAY_MS                = const(5)
+BMX160_GYRO_DELAY_MS                 = const(81)
+BMX160_ONE_MS_DELAY                  = const(1)
+BMX160_MAG_COM_DELAY                 = const(10)
+BMX160_GYRO_SELF_TEST_DELAY          = const(20)
+BMX160_ACCEL_SELF_TEST_DELAY         = const(50)
+
+# Output Data Rate settings
+# Accel Output data rate
+BMX160_ACCEL_ODR_RESERVED            = const(0x00)
+BMX160_ACCEL_ODR_0_78HZ              = const(0x01)
+BMX160_ACCEL_ODR_1_56HZ              = const(0x02)
+BMX160_ACCEL_ODR_3_12HZ              = const(0x03)
+BMX160_ACCEL_ODR_6_25HZ              = const(0x04)
+BMX160_ACCEL_ODR_12_5HZ              = const(0x05)
+BMX160_ACCEL_ODR_25HZ                = const(0x06)
+BMX160_ACCEL_ODR_50HZ                = const(0x07)
+BMX160_ACCEL_ODR_100HZ               = const(0x08)
+BMX160_ACCEL_ODR_200HZ               = const(0x09)
+BMX160_ACCEL_ODR_400HZ               = const(0x0A)
+BMX160_ACCEL_ODR_800HZ               = const(0x0B)
+BMX160_ACCEL_ODR_1600HZ              = const(0x0C)
+BMX160_ACCEL_ODR_RESERVED0           = const(0x0D)
+BMX160_ACCEL_ODR_RESERVED1           = const(0x0E)
+BMX160_ACCEL_ODR_RESERVED2           = const(0x0F)
+
+# Gyro Output data rate
+BMX160_GYRO_ODR_RESERVED             = const(0x00)
+BMX160_GYRO_ODR_25HZ                 = const(0x06)
+BMX160_GYRO_ODR_50HZ                 = const(0x07)
+BMX160_GYRO_ODR_100HZ                = const(0x08)
+BMX160_GYRO_ODR_200HZ                = const(0x09)
+BMX160_GYRO_ODR_400HZ                = const(0x0A)
+BMX160_GYRO_ODR_800HZ                = const(0x0B)
+BMX160_GYRO_ODR_1600HZ               = const(0x0C)
+BMX160_GYRO_ODR_3200HZ               = const(0x0D)
+
+# Auxiliary sensor Output data rate
+BMX160_AUX_ODR_RESERVED              = const(0x00)
+BMX160_AUX_ODR_0_78HZ                = const(0x01)
+BMX160_AUX_ODR_1_56HZ                = const(0x02)
+BMX160_AUX_ODR_3_12HZ                = const(0x03)
+BMX160_AUX_ODR_6_25HZ                = const(0x04)
+BMX160_AUX_ODR_12_5HZ                = const(0x05)
+BMX160_AUX_ODR_25HZ                  = const(0x06)
+BMX160_AUX_ODR_50HZ                  = const(0x07)
+BMX160_AUX_ODR_100HZ                 = const(0x08)
+BMX160_AUX_ODR_200HZ                 = const(0x09)
+BMX160_AUX_ODR_400HZ                 = const(0x0A)
+BMX160_AUX_ODR_800HZ                 = const(0x0B)
+
+# Accel, gyro and aux. sensor length and also their combined length definitions in FIFO
+BMX160_FIFO_G_LENGTH                 = const(6)
+BMX160_FIFO_A_LENGTH                 = const(6)
+BMX160_FIFO_M_LENGTH                 = const(8)
+BMX160_FIFO_GA_LENGTH                = const(12)
+BMX160_FIFO_MA_LENGTH                = const(14)
+BMX160_FIFO_MG_LENGTH                = const(14)
+BMX160_FIFO_MGA_LENGTH               = const(20)
 
 # I2C address
-# BMX160_I2C_ADDR            = const(0x68)
-BMX160_I2C_ADDR            = const(0x69)  # alternate address, this one seems to work!
+BMX160_I2C_ADDR            = const(0x68)
+BMX160_I2C_ALT_ADDR        = const(0x69)  # alternate address
 # Interface settings
 BMX160_SPI_INTF            = const(1)
 BMX160_I2C_INTF            = const(0)
@@ -114,10 +174,11 @@ class BMX160:
     Driver for the BMX160 accelerometer, magnetometer, gyroscope.
 
     In the buffer, bytes are allocated as follows:
-        - mag x6
-        - rhall x2 (not relevant)
-        - gyro x6
-        - accel x6
+        - mag 0-5
+        - rhall 6-7 (not relevant?)
+        - gyro 8-13
+        - accel 14-19
+        - sensor time 20-22
 
     """
 
@@ -165,16 +226,17 @@ class BMX160:
         like ("mag", "gyro", "accel"), and ("range", "config"). Case doesn't matter for these strings.
         """
         assert sensor.lower() in ("mag", "gyro", "accel")
-        if param.lower() != "range":
-            param = "config" #TODO enumerate allowed keywords.
 
-        # registername = "BMX160_" + sensor.upper() + "_" + param.upper() + "_ADDR"
-        registername = "BMX160_COMMAND_REG_ADDR"
+        if param.lower() == "range":
+            registername = "BMX160_" + sensor.upper() + "_RANGE_ADDR"
+        else:
+            registername = "BMX160_COMMAND_REG_ADDR"
+
         if registername in globals():
             register = globals()[registername]
             self.write_u8(register, value)
         else:
-            print("WARNING: no register found corresponding to setting {}".format(param.upper()))
+            print("WARNING: no register found corresponding to {}".format(registername))
 
     def apply_sensor_params(self, settings = None):
         if settings == None:
@@ -209,7 +271,14 @@ class BMX160:
                          # "range": BMX160_GYRO_RANGE_2000_DPS
                          }
 
-        return {"accel": accel_settings, "gyro": gyro_settings}
+        mag_settings = {
+                         "bw": BMX160_MAG_BW_NORMAL_MODE,
+                         "odr": BMX160_MAG_ODR_100HZ,
+                         "power": BMX160_MAG_NORMAL_MODE,
+                         # "range": BMX160_GYRO_RANGE_2000_DPS
+                         }
+
+        return {"accel": accel_settings, "gyro": gyro_settings, "mag": mag_settings}
 
 
 
@@ -218,20 +287,23 @@ class BMX160_I2C(BMX160):
     """Driver for the BMX160 connect over I2C."""
 
     def __init__(self, i2c):
-        self.i2c_device = I2CDevice(i2c, BMX160_I2C_ADDR)
+        try:
+            self.i2c_device = I2CDevice(i2c, BMX160_I2C_ADDR)
+        except:
+            self.i2c_device = I2CDevice(i2c, BMX160_I2C_ALT_ADDR)
         super().__init__()
 
     def read_u8(self, address):
         with self.i2c_device as i2c:
             self._BUFFER[0] = address & 0xFF
             i2c.write_then_readinto(self._BUFFER, self._BUFFER, out_end=1, in_start=1, in_end=2)
-        return self._BUFFER[1]
+        return self._BUFFER[address]
 
     def read_bytes(self, address, count, buf):
         with self.i2c_device as i2c:
             buf[0] = address & 0xFF
             i2c.write_then_readinto(buf, buf, out_end=1, in_end=count)
-
+        return buf
     def write_u8(self, address, val):
         with self.i2c_device as i2c:
             self._BUFFER[0] = address & 0xFF
@@ -250,13 +322,14 @@ class BMX160_SPI(BMX160):
             self._BUFFER[0] = (address | 0x80) & 0xFF
             spi.write(self._BUFFER, end=1)
             spi.readinto(self._BUFFER, end=1)
-        return self._BUFFER[0]
+        return self._BUFFER[address]
 
     def read_bytes(self, address, count, buf):
         with self.i2c_device as spi:
             buf[0] = (address | 0x80) & 0xFF
             spi.write(buf, end=1)
             spi.readinto(buf, end=count)
+        return buf
 
     def write_u8(self, address, val):
         with self.i2c_device as spi:
